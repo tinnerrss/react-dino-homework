baced7a3d338656b057f7f1d1c136c514cf1157c
tinnerrss <tinnerrss@hotmail.com>
2020-03-04 13:31:36 -0800

    bleh

--- src/App.js
+++ src/App.js
@@ -1,49 +1,97 @@
-import React, { Component } from "react";
-import "./styles.css";
+import React, {Component} from 'react';
+import './App.css';
 
 let elems = {
-  postTitle: "Dinosaurs are awesome",
-  author: "By Stealthy Stegosaurous",
-  content: [
-    "Check out this body property",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-  ],
-  comment: ["First!", "Second", "Third"]
-};
-class PostTitle extends Component {
-  render() {
-    return <h1>{this.props.postTitle}</h1>;
-  }
-}
-class Author extends Component {
-  render() {
-    return <h3>{this.props.author}</h3>;
-  }
-}
-class Content extends Component {
-  render() {
-    return <h1>{this.props.content}</h1>
-  }
-}
-class Comment extends Component {
-  render() {
-    return <h3>{this.props.comment}</h3>;
-  }
-}
-class Post extends Component {
-  render() {
-    return (
-      <div>
-        <PostTitle postTitle={elems.postTitle} />
-        <Author author={elems.author} />
-        <Content content={elems.content} />
-        <Comment comment={elems.comment} />
-      </div>
-    );
-  }
-}
-export default function App() {
-  return <Post />;
+    header: 'Dinos be dinoin',
+    author: 'Richard Scary',
+    content: 'THIS IS A POST ABOUT DINOS LISTEN UP PEOPLE'
 }
 
+let commentStuff = [{
+    commentHeader: 'Rawr',
+    commentBody: 'Dinobama says hi!'
+  }, 
+  {
+    commentHeader: 'Radasaurous',
+    commentBody: 'RAWRAWRAWR'
+  }]
+
+class Header extends Component {
+    render () {
+        return (
+            <div>
+                <h1>{this.props.header}</h1>
+                <h3>{this.props.author}</h3>
+            </div>
+        )
+    }
+}
+class Content extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            body: this.props.content
+        }
+        this.changeContent = this.changeContent.bind(this)
+    }
+    changeContent(e) {
+        //render alert on click
+        let answer = prompt("What would you like to update the body to?") ;
+        //update state
+        this.setState((prevState, props) => {
+            return {
+                body: answer
+            }
+        })
+    }
+
+    render () {
+        return (
+            <div>
+                <p>{this.state.body}</p>
+                <button onClick={this.changeContent}>Click to Edit</button>
+            </div>
+        )
+    }
+}
+  
+  class Comment extends Component {
+    render () {
+        return (
+            <div>
+                <h3>{this.props.commentHeader}</h3>
+                <p>{this.props.commentBody}</p>
+            </div>
+        )
+    }
+  }
+  class Commentpost extends Component {
+    render () {
+        let comments = [];
+        commentStuff.forEach(function(comment,index) {
+            comments.push(
+            <Comment 
+            commentHeader={comment.commentHeader}
+            commentBody={comment.commentBody}
+            />
+            );
+        })
+        return (
+            <div>
+                {comments}
+            </div>
+        )
+    }
+  }
+
+  function App() {
+      return (
+          <div>
+              <Header header={elems.header} author={elems.author} />
+              <Content content={elems.content}/>
+              <Commentpost />
+          </div>
+      )
+  }
+
+  export default App;
